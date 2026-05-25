606a8c6037ee6d3feda8d2e72f310ce6192cbaca
Jacob <acostalolgreatjcc@gmail.com>
2026-05-25 19:23:01 +0800

    feat(landing)/ Upload Buttons

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
+import AnswerKeyButton from "@/components/Landing/AnswerKeyButton";
+import AnswerSheetButton from "@/components/Landing/AnswerSheetButton";
 import Logo from "@/components/Logo";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div
-      className={`flex min-h-screen items-center justify-center font-sans dark:bg-black`}
-    >
-      <Logo></Logo>
+    <div className="min-h-screen">
+      <div className="absolute"><Logo /></div>
+      <div
+        className={`min-h-screen flex items-center justify-center font-sans dark:bg-black`}
+      >
+        <div className="flex">
+
+          <AnswerSheetButton />
+
+          <AnswerKeyButton />
+
+        </div>
+      </div>
     </div>
   );
+
 }
