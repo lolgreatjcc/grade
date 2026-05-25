a4b5d9fa8680d5058f8ba199a180528f15c66bdc
Jacob <acostalolgreatjcc@gmail.com>
2026-05-26 00:48:20 +0800

    feat(landing)/ Add menu button

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ import styles from './index.module.css';
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="absolute"><Logo /></div>
-      <div className="absolute">
-        
+      <div className="absolute left-10"><Logo /></div>
+      <div className="absolute right-5 top-5">
 
+        <svg className={`${styles.menuBtn}`} xmlns="http://www.w3.org/2000/svg" height="64px" viewBox="0 -960 960 960" width="64px">
+          <path fill="currentColor" d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+        </svg>
       </div>
       <div className="absolute bottom-10 right-10">
         <h2 className={`${styles.tutorialBtnTxt} text-2xl`}>Need a tutorial?</h2>
