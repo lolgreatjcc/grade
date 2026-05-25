9bbb5ae41dcad56cdbd21176cdf1816a2a9f283c
Jacob <acostalolgreatjcc@gmail.com>
2026-05-26 00:35:31 +0800

    feat(landing)/ Add tutorial btn

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,19 @@ import AnswerKeyButton from "@/components/Landing/AnswerKeyButton";
 import AnswerSheetButton from "@/components/Landing/AnswerSheetButton";
 import GradeButton from "@/components/Landing/GradeButton";
 import Logo from "@/components/Logo";
+import styles from './index.module.css';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <div className="absolute"><Logo /></div>
+      <div className="absolute">
+        
+
+      </div>
+      <div className="absolute bottom-10 right-10">
+        <h2 className={`${styles.tutorialBtnTxt} text-2xl`}>Need a tutorial?</h2>
+      </div>
       <div
         className={`min-h-screen flex items-center justify-center flex-col`}
       >
@@ -20,6 +28,7 @@ export default function Home() {
 
         <GradeButton />
       </div>
+
 
     </div>
   );
