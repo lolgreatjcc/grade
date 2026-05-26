081341eb150b993b51e6b49b0f09b4fa5e66e722
Jacob <acostalolgreatjcc@gmail.com>
2026-05-26 16:18:17 +0800

    feat(grade)/ Add grade page

--- components/Landing/GradeButton.js
+++ components/Landing/GradeButton.js
@@ -1,5 +1,6 @@
 ﻿import { Ponomar } from "next/font/google"
 import styles from './LandingButtons.module.css';
+import { useRouter } from "next/router";
 const ponomar = Ponomar({
   subsets: ['latin'],
   weight: '400'
@@ -7,10 +8,11 @@ const ponomar = Ponomar({
 
 export default function GradeButton() {
 
+  const router = useRouter();
+
   return (
-    <div className={`${styles.gradeButton} px-20 py-4 rounded-sm`}>
+    <div className={`${styles.gradeButton} px-20 py-4 rounded-sm`} onClick={() => router.push('./grade')}>
       <h2 className={`${ponomar.className} text-2xl ${styles.gradeButtonText}`}>grade</h2>
     </div>
   )
-
 }
---
+++ pages/grade/index.js
@@ -0,0 +1,11 @@
+﻿import MarkSection from "@/components/Grade/MarkSection";
+
+
+export default function Grade() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <MarkSection />
+    </div>
+  );
+
+}
