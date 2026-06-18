be3e2d683ae61ffecd404760bb49d499ce93c9bc
Jacob <acostalolgreatjcc@gmail.com>
2026-06-19 00:32:33 +0800

    fe-feat(tooltips)/hoverQnStore

---
+++ stores/tooltip/HoveredQnStore.js
@@ -0,0 +1,11 @@
+﻿'use client'
+import { create } from 'zustand';
+
+
+const useHoveredQnStore = create((set) => ({
+  hoveredQn: null,
+  setHoveredQn: (newHoveredQn) => set({ hoveredQn: newHoveredQn }),
+  clearHoveredQn: () => set({ hoveredQn: null })
+}));
+
+export default useHoveredQnStore;
