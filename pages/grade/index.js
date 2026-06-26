131bcb81aff87582208b1c0f4d5c121d6e3f2f81
Jacob Acosta <87067973+lolgreatjcc@users.noreply.github.com>
2026-06-26 20:36:04 +0800

    Change API endpoint from '/marking' to '/grade/marking'

--- pages/grade/index.js
+++ pages/grade/index.js
@@ -92,7 +92,7 @@ export default function Grade() {
         pageImage: currentImage
       }
 
-      axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/marking`, requestBody).then(result => {
+      axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/grade/marking`, requestBody).then(result => {
 
 
         const newMarkedData = markedData;
