ebcf99fd480ba585bd1f0949289fdfef4c5b62dd
Jacob <acostalolgreatjcc@gmail.com>
2026-06-28 14:30:24 +0800

    be-feat/add comments that explain what each endpoint does

--- controller/sheetGen.js
+++ controller/sheetGen.js
@@ -25,6 +25,7 @@ const upload = multer({
 })
 
 
+// Finds the number of MCQs and FRQs a pdf file has.
 router.post('/sheetGen', upload.single('file'), async (req, res) => {
 
   const inputQnSheetFile = req.file;
