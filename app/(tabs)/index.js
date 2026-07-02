350dabf84a7d3f1cb2974e525f05cb8bf6c98890
Jacob <acostalolgreatjcc@gmail.com>
2026-07-02 23:44:16 +0800

    mb-feat/Initialized Expo Mobile

---
+++ app/(tabs)/index.js
@@ -0,0 +1,27 @@
+﻿import { Image } from 'expo-image';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Logo from '../../components/Landing/Logo';
+import GradientText from '../../components/GradientText';
+import HomeGradient from '../../components/Landing/HomeGradient';
+
+
+export default function LandingScreen() {
+  return (
+    <HomeGradient>
+
+      <Logo />
+
+      <View style={styles.answerSheetParent}> 
+        <Text>test</Text>
+      </View>
+
+    </HomeGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  answerSheetParent: {
+    backgroundColor: '#85603C'
+  } 
+});
---
+++ app/_layout.js
@@ -0,0 +1,28 @@
+import { Ponomar_400Regular } from '@expo-google-fonts/ponomar/400Regular';
+import { useFonts } from '@expo-google-fonts/ponomar/useFonts';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+
+  let [fontsLoaded] = useFonts({
+    Ponomar_400Regular
+  })
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
