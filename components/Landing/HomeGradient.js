b4c97dd2f530d49e08beb92447baf52eed62844e
Jacob <acostalolgreatjcc@gmail.com>
2026-07-02 23:45:23 +0800

    mb-feat/added home gradient

---
+++ components/Landing/HomeGradient.js
@@ -0,0 +1,23 @@
+﻿import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet } from "react-native";
+
+
+export default function HomeGradient(props) {
+
+
+  return (
+    <LinearGradient style={styles.landingContainer} colors={['#85603C', '#2A1F13']}
+      start={[0, 0]} end={[1, 1]}
+    >
+      {props.children}
+    </LinearGradient>
+  )
+}
+
+const styles = StyleSheet.create({
+  landingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
