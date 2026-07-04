98e5e8718cccb8fea313c9211558ac505ba32ea4
Jacob <acostalolgreatjcc@gmail.com>
2026-07-04 20:00:52 +0800

    mb-feat(landing)/Added 'grade' button

---
+++ components/Landing/GradeButton.js
@@ -0,0 +1,39 @@
+﻿import { StyleSheet, Text, View } from "react-native";
+import GradientText from "../GradientText";
+import Logo from "./Logo";
+
+
+
+export default function GradeButton() {
+
+  return (
+    <View style={styles.parentContainer}>
+      <View style={styles.buttonContainer}>
+        <GradientText text={'grade'} textStyle={styles.textDesign} colors={['#BAA678', '#E7E0DE']} />
+      </View>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    width: '100%',
+  },
+  buttonContainer: {
+    borderRadius: 15,
+    backgroundColor: '#2A1F13',
+    marginLeft: 15,
+    marginRight: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  textDesign: {
+    fontSize: 32,
+    letterSpacing: -1,
+    fontFamily: "Ponomar_400Regular"
+  }
+})
