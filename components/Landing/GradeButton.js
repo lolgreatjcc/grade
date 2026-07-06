4efae23753bd05b5cc0b994ed86e644277a5029e
Jacob <acostalolgreatjcc@gmail.com>
2026-07-07 02:03:29 +0800

    mb-feat/added backend request for '/grade'

--- components/Landing/GradeButton.js
+++ components/Landing/GradeButton.js
@@ -1,17 +1,59 @@
-﻿import { StyleSheet, Text, View } from "react-native";
+﻿import { Button, Pressable, StyleSheet, Text, Touchable, TouchableWithoutFeedback, View } from "react-native";
 import GradientText from "../GradientText";
 import Logo from "./Logo";
+import useFileStore from "../../store/landing/fileStore";
+import { File, Directory, Paths } from 'expo-file-system';
+import axios from "axios";
+
 
 
 
 export default function GradeButton() {
 
+  const answerSheet = useFileStore((state) => state.answerSheet);
+  const answerKey = useFileStore((state) => state.answerKey);
+
+
+  const acceptableFiles = (() => {
+    if (answerKey === null || answerSheet === null) return false;
+    if (answerKey.acceptable === false || answerSheet.acceptable === false) return false;
+    return true
+  })();
+
+
+
+  async function grade() {
+    console.log(acceptableFiles);
+    if (!acceptableFiles) return true
+
+    
+    const formData = new FormData();
+
+    formData.append('files', {
+      uri: answerSheet.uri,
+      name: answerSheet.name,
+      type: "application/pdf"
+    });
+    formData.append('files', {
+      uri: answerKey.uri,
+      name: answerSheet.name,
+      type: "application/pdf"
+    });
+
+
+    const response = await axios.post(`${process.env.EXPO_PUBLIC_API_URL}/grade`, formData).then((result) => {
+      console.log(result);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
-    <View style={styles.parentContainer}>
+    <Pressable style={styles.parentContainer} onPress={grade}>
       <View style={styles.buttonContainer}>
         <GradientText text={'grade'} textStyle={styles.textDesign} colors={['#BAA678', '#E7E0DE']} />
       </View>
-    </View>
+    </Pressable>
   )
 }
 
