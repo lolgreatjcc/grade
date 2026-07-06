882a8b6694f12e791aed37016138ead1877a591e
Jacob <acostalolgreatjcc@gmail.com>
2026-07-07 02:01:48 +0800

    mb-feat/add base UI

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -6,17 +6,43 @@ import GradientText from '../../components/GradientText';
 import HomeGradient from '../../components/Landing/HomeGradient';
 import GradeButton from '../../components/Landing/GradeButton';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import FileButton from '../../components/Landing/FileButton';
+import { useEffect, useState } from 'react';
+import useFileStore from '../../store/landing/fileStore';
 
 
 export default function LandingScreen() {
+
+
+  const [highlight, setHighlight] = useState('Sheet');
+
+  const sheetWidth = highlight == 'Sheet' ? '75%' : '40%'
+  const keyWidth = highlight == 'Key' ? '65%' : '40%'
+
+  const setAnswerSheet = useFileStore((state) => state.setAnswerSheet);
+  const setAnswerKey = useFileStore((state) => state.setAnswerKey);
+
+
+  
+
+
   return (
     <HomeGradient>
       <SafeAreaView style={styles.safeAreaView}>
 
-        <View>
-          <Logo />
+        <Logo />
+
+
+
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch' }}>
+          <View style={{ flexGrow: 3, justifyContent: 'center', alignItems: 'center' }}>
+            <FileButton text='Answer Sheet' width={sheetWidth} setFile={setAnswerSheet}/>
+          </View>
+          <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <FileButton text='Answer Key' width={keyWidth} setFile={setAnswerKey}/>
+          </View>
         </View>
-        <View style={{ flex: 1 }}></View>
 
 
         <GradeButton />
@@ -30,8 +56,7 @@ const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
     width: '100%',
-    paddingLeft: 15,
-    paddingRight: 15,
+    paddingHorizontal: 15,
   },
   answerSheetParent: {
     backgroundColor: '#85603C'
