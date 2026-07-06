eb4ab3b82f8b3a716cab20076f640cf07fa7ba61
Jacob <acostalolgreatjcc@gmail.com>
2026-07-07 02:01:24 +0800

    fe-feat/Add 'Text' reusable text component using RedditMono font

---
+++ components/Text.js
@@ -0,0 +1,16 @@
+﻿import { StyleSheet } from "react-native"
+
+import { Text as NativeText } from "react-native"
+
+
+
+export default function Text({ style, ...props}) {
+  return <NativeText style={[styles.globalFont, style]} {...props} /> 
+}
+
+const styles = StyleSheet.create({
+  globalFont: {
+    fontFamily: "Raleway_400Regular"
+
+  }
+})
