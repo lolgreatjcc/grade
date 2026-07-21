813ff0b7ac27234f69a1e480089aa75182f4ad9c
Jacob <acostalolgreatjcc@gmail.com>
2026-07-21 09:27:54 +0800

    mb-feat/Add fontWeight func for custom Text component

--- components/Text.js
+++ components/Text.js
@@ -4,13 +4,33 @@ import { Text as NativeText } from "react-native"
 
 
 
-export default function Text({ style, ...props}) {
-  return <NativeText style={[styles.globalFont, style]} {...props} /> 
+export default function Text({ style, ...props }) {
+  const flattenedStyle = StyleSheet.flatten(style) ?? {};
+  const fontWeight = flattenedStyle.fontWeight;
+
+  if (!fontWeight) {
+    return <NativeText style={[styles.regularWeight, style]} {...props} />
+
+  }
+
+
+  if (style.fontWeight == 'medium') {
+    return <NativeText style={[styles.mediumWeight, style]} {...props} />
+  } else if (style.fontWeight == 'bold') {
+    return <NativeText style={[styles.boldWeight, style]} {...props} />
+  } else {
+    return <NativeText style={[styles.regularWeight, style]} {...props} />
+  }
 }
 
 const styles = StyleSheet.create({
-  globalFont: {
+  regularWeight: {
     fontFamily: "Raleway_400Regular"
-
+  },
+  mediumWeight: {
+    fontFamily: "Raleway_500Medium"
+  },
+  boldWeight: {
+    fontFamily: "Raleway_700Bold"
   }
 })
