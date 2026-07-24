b297e0abf839295b29aa063b362d5fa2a3e20d98
TYRPRO <www.yongrui@gmail.com>
2026-07-25 00:52:09 +0800

    added hover bg-colour

--- components/Common/FileUpload/ItemSelect.js
+++ components/Common/FileUpload/ItemSelect.js
@@ -37,7 +37,8 @@ export default function ItemSelect({selectedFileId, setSelectedFileId, file, fet
 
   return (
     <div className="cursor-pointer mx-4">
-      <div className={`flex items-center py-2 pl-2 ${isSelected ? styles.activeButton : ''}`} onClick={handleOnClick}>
+      <div className={`flex items-center py-2 pl-2 hover:bg-[#81592f] transition duration-180 ease-in-out
+        ${isSelected ? styles.activeButton : ''}`} onClick={handleOnClick}>
         <Image className="w-[16px] h-[16px]" alt="img" width={16} height={16} src={file.icon}/>
         <h1 className={"ml-2"}>{file.name}</h1>
         {isLoading && <h1 className={`${styles.unselected} ml-2`}>(Loading...)</h1>}
