33c04ae970c68ad2270a6775a2a4666e211f0381
Jacob <acostalolgreatjcc@gmail.com>
2026-08-12 20:10:35 +0800

    fe-bug/unified coloring of supplement question paper button

--- components/Landing/ToggleButton.js
+++ components/Landing/ToggleButton.js
@@ -11,11 +11,11 @@ export default function ToggleButton({setState, text}) {
   return (
     <label className="inline-flex items-center cursor-pointer my-4">
         <input type="checkbox" value="" className="sr-only peer" onChange={handleCheckbox}/>
-        <span className="select-none ms-3 text-sm font-medium text-heading mr-3 text-[#FFFFFFCC]">{text}</span>
-        <div className="relative w-9 h-5 bg-black peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft 
-        dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
+        <span className="select-none ms-3 text-sm text-heading mr-3 text-[#FFFFFFCC]">{text}</span>
+        <div className={` ${styles.supplementBackground} relative w-9 h-5 rounded-full peer peer-checked:after:translate-x-full 
+        rtl:peer-checked:after:-translate-x-full 
         peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[#FFFFFFCC] after:rounded-full 
-        after:h-4 after:w-4 after:transition-all peer-checked:bg-[#2A1F13]"></div>
+        after:h-4 after:w-4 after:transition-all peer-checked:bg-[#1c150d]`}></div>
     </label>
   )
 
